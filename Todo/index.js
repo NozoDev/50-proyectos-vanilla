5a7adf012e9534829aff50571caac83d538703dd
NozoDev <kevinroj326@gmail.com>
2024-04-16 23:12:56 -0500

    aplicando la funcionalidad de completado y estilos un poco feo pero alli vamos

--- Todo/index.js
+++ Todo/index.js
@@ -1,10 +1,10 @@
 //Paso 1. Obtener elementos del dom
-const addBtn = document.getElementById("agregar-tarea");
+const form = document.getElementById("form");
 const input = document.getElementById("tarea");
 const taskList = document.getElementById("list");
 
 // paso 2. escuchar el evento de envio del formulario
-addBtn.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault(); // evita que se recargue la pagina
   addTask();
 });
@@ -15,22 +15,21 @@ function addTask() {
   const taskText = input.value.trim(); // esto obtiene el valor del campo de text y elimina espacios en blanco
   if (taskText !== "") {
     const taskItem = document.createElement("li");
-
+    taskItem.classList.add("task");
     taskItem.textContent = taskText;
-    taskItem.classList.add("list");
     taskList.appendChild(taskItem);
-
     input.value = ""; // limpiar el campo de texto despues de agregar la tarea
   }
 }
 
 // marcar tareas como completadas
+
 taskList.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
-    completedTodo(e.target);
+    toggleTaskCompletion(e.target);
   }
 });
 
-function completedTodo(taskItem) {
+function toggleTaskCompletion(taskItem) {
   taskItem.classList.toggle("completed");
 }
