c54f24596daa3666f9bf3003debdf1f32c6f881c
NozoDev <kevinroj326@gmail.com>
2024-04-16 22:17:34 -0500

    agregue unos estilos y una funcionalidad de agregar y marcar como completado, estos estilos lo estoy haciendo a pulso si nninguna figma pues

--- Todo/index.js
+++ Todo/index.js
@@ -1,10 +1,10 @@
 //Paso 1. Obtener elementos del dom
-const form = document.getElementById("form");
+const addBtn = document.getElementById("agregar-tarea");
 const input = document.getElementById("tarea");
 const taskList = document.getElementById("list");
 
 // paso 2. escuchar el evento de envio del formulario
-form.addEventListener("submit", function (e) {
+addBtn.addEventListener("click", function (e) {
   e.preventDefault(); // evita que se recargue la pagina
   addTask();
 });
@@ -15,8 +15,22 @@ function addTask() {
   const taskText = input.value.trim(); // esto obtiene el valor del campo de text y elimina espacios en blanco
   if (taskText !== "") {
     const taskItem = document.createElement("li");
+
     taskItem.textContent = taskText;
+    taskItem.classList.add("list");
     taskList.appendChild(taskItem);
+
     input.value = ""; // limpiar el campo de texto despues de agregar la tarea
   }
+}
+
+// marcar tareas como completadas
+taskList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    completedTodo(e.target);
+  }
+});
+
+function completedTodo(taskItem) {
+  taskItem.classList.toggle("completed");
 }
