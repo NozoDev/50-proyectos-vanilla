10b9ec4f1c90fb240fdf228a4000ea5dc2deff0f
NozoDev <kevinroj326@gmail.com>
2024-04-16 21:39:53 -0500

    agregando un pequeño html para dar funcionalidad y luego empezar aplicar estilos de css

---
+++ Todo/index.js
@@ -0,0 +1,6 @@
+//Paso -1 :Obtener elementos del dom
+const form = document.getElementById("form");
+const input = document.getElementById("tarea");
+const taskList = document.getElementById("list");
+
+form.addEventListener("submit", function (e) {});
